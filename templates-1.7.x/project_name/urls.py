d212b4bc1872d3b19c6e3e59af7fcb3d3032177b
Aitzol Naberan <anaberan@codesyntax.com>
2014-12-10 16:37:56 +0100

    Remove old attemps

--- templates-1.7.x/project_name/urls.py
+++
@@ -1,7 +0,0 @@
- from django.conf.urls import patterns, include, url
-
- urlpatterns = patterns('',
-    #To completely get openlivinlab.com in your environment
-    #uncomment this line
-    #url(r('', include('smartlivinglab.urls')),
- )
