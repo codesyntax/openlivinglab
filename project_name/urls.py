8ebee82308b07b3208e18b55872be34cd8fc8a24
Aitzol Naberan <anaberan@codesyntax.com>
2014-12-10 17:04:04 +0100

    Fix url definition

--- project_name/urls.py
+++ project_name/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     #To completely get openlivinlab.com in your environment
     #uncomment this line
-    #url(r('', include('smartlivinglab.urls')),
+    #url(r'', include('smartlivinglab.urls')),
 )
