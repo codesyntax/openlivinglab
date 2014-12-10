b36becf169777c0975d20e3a8268bd8daaea6b3c
Aitzol Naberan <anaberan@codesyntax.com>
2014-12-10 16:34:28 +0100

    Reordering

---
+++ project_name/urls.py
@@ -0,0 +1,7 @@
+ from django.conf.urls import patterns, include, url
+
+ urlpatterns = patterns('',
+    #To completely get openlivinlab.com in your environment
+    #uncomment this line
+    #url(r('', include('smartlivinglab.urls')),
+ )
